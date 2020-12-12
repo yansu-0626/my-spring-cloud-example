9d648284d5cd84b34a56400cdc7e383fdc4ce6fc
yans <yansu_1@qq.com>
2020-12-12 23:00:40 +0800

    1

--- msc-common-log-starter/src/main/java/com/su/myspringboot/msccommonlog/MscCommonLogStarterApplication.java
+++ msc-common-log-starter/src/main/java/com/su/myspringboot/msccommonlog/MscCommonLogStarterApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MscCommonLogApplication {
+public class MscCommonLogStarterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MscCommonLogApplication.class, args);
+        SpringApplication.run(MscCommonLogStarterApplication.class, args);
     }
 
 }
---
+++ msc-common-log-starter/src/main/java/com/su/myspringboot/msccommonlog/service/TestService.java
@@ -0,0 +1,11 @@
+package com.su.myspringboot.msccommonlog.service;
+
+/**
+ * @ClassName TestService
+ * @Description TODO
+ * @Author yansu
+ * @Date 2020/12/12 下午 10:52
+ * @Version 1.0
+ **/
+public class TestService {
+}
